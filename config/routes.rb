07528a1ddb7aab93bb089a6d8a8e194c7a59ea76
EC2 Default User <ec2-user@ip-172-31-37-87.ap-northeast-1.compute.internal>
2020-03-18 09:35:26 +0000

    投稿一覧ページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,11 @@
 Rails.application.routes.draw do
-  get 'posts/new'
-
-  get 'posts/create'
-
   devise_for :users,
     controllers: { registrations: 'registrations' }
     
-  root 'pages#home'
+  root 'posts#index'
   get  '/users/:id', to: 'users#show', as: 'user'
   
-  resources :posts, only: %i(new create) do
+  resources :posts, only: %i(new create index) do
     resources :photos, only: %i(create)
   end
 end
