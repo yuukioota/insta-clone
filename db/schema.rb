18cca8b84c47f9f7a83a30335648929953bf3888
EC2 Default User <ec2-user@ip-172-31-37-87.ap-northeast-1.compute.internal>
2020-03-19 02:06:39 +0000

    コメント機能の実装の準備(Commentモデルの作成と関連付け)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200319012810) do
+ActiveRecord::Schema.define(version: 20200319020228) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "post_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "likes", force: :cascade do |t|
     t.integer "post_id", null: false
