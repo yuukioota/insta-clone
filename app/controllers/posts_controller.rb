d4833b9ede340a59326e2eff8eb7a7f310699b0e
EC2 Default User <ec2-user@ip-172-31-37-87.ap-northeast-1.compute.internal>
2020-03-24 01:53:10 +0000

    POST詳細ページのデバッグ

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,5 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_post, only: %i(show destroy)
   
   def index
     @posts = Post.limit(10).includes(:photos, :user).order('created_at DESC')
@@ -24,24 +23,11 @@ class PostsController < ApplicationController
   end
   
   def show
-    
-  end
-  
-  def destroy
-    if @post.user == current_user
-      flash[:notice] = "投稿が削除されました" if @post.destroy
-    else
-      flash[:alert] = "投稿の削除に失敗しました"
-    end
-    redirect_to root_path
+    @post = Post.find_by(id: params[:id])
   end
   
   private
     def post_params
       params.require(:post).permit(:caption, photos_attributes: [:image]).merge(user_id: current_user.id)
-    end
-    
-    def set_post
-      @post = Post.find_by(id: params[:id])
     end
 end
--- db/migrate/20200320040422_add_comments_to_comments.rb
+++ db/migrate/20200320040422_add_comments_to_comments.rb
@@ -2,5 +2,6 @@ class AddCommentsToComments < ActiveRecord::Migration[5.1]
   def change
     add_column :comments, :comment, :string
     change_column :comments, :comment, :string, :null => false
+    
   end
 end
